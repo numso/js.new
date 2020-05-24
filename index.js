41c59b3775f1090e656ba6aea630fabe583be2e0
Dallin Osmun <dosmun@gmail.com>
2020-05-24 14:21:22 -0600

    (0.0.1) remove top level await

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --experimental-top-level-await
+#!/usr/bin/env node
 import fs from 'fs'
 import http from 'http'
 import path from 'path'
@@ -7,25 +7,30 @@ import babel from '@babel/core'
 import jsx from '@babel/plugin-transform-react-jsx'
 
 const [, , srcDir = '.'] = process.argv
-const mimedb = { '.html': 'text/html', '.js': 'application/javascript', '.jsx': 'application/javascript' }
-await lexer.init
+const mimedb = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.jsx': 'application/javascript'
+}
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   const fullPath = findFile(req.url === '/' ? '/index.html' : req.url)
   if (!fullPath) return res.writeHead(404).end()
   let contents = fs.readFileSync(fullPath, 'utf8')
   const mime = mimedb[path.extname(fullPath)]
   if (!mime) console.warn(`missing mime type for ${path.extname(fullPath)}`)
   if (mime === 'application/javascript') {
-    contents = babel.transformSync(contents, { filename: fullPath, plugins: [jsx] }).code
-    lexer.parse(contents, fullPath)[0].reverse().forEach(({ s, e }) => {
+    const options = { filename: fullPath, plugins: [jsx] }
+    contents = babel.transformSync(contents, options).code
+    const imports = lexer.parse(contents, fullPath)[0].reverse()
+    imports.forEach(({ s, e }) => {
       const name = contents.substring(s, e)
       contents = contents.slice(0, s) + updateDep(name) + contents.slice(e)
     })
   }
   if (mime) res.setHeader('Content-Type', mime)
   res.end(contents)
-}).listen(3000, () => console.log('Listening on 3000'))
+})
 
 function findFile (url) {
   const base = path.join(process.cwd(), srcDir, url)
@@ -37,4 +42,8 @@ function findFile (url) {
 }
 
 const checkFile = p => fs.existsSync(p) && fs.lstatSync(p).isFile()
-const updateDep = n => n[0] === '.' ? n : `https://cdn.pika.dev/${n}`
+const updateDep = n => (n[0] === '.' ? n : `https://cdn.pika.dev/${n}`)
+
+lexer.init.then(() =>
+  server.listen(3000, () => console.log('Listening on 3000'))
+)
