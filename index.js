ee65b522aba631deea2ad9e95654054d6767d91c
Dallin Osmun <dosmun@gmail.com>
2020-05-24 16:17:36 -0600

    (0.0.2) add json support. fix /index.js inference. support class properties syntax.

--- index.js
+++ index.js
@@ -5,44 +5,47 @@ import path from 'path'
 import lexer from 'es-module-lexer'
 import babel from '@babel/core'
 import jsx from '@babel/plugin-transform-react-jsx'
+import cp from '@babel/plugin-syntax-class-properties'
 
-const [, , srcDir = '.'] = process.argv
-const mimedb = {
-  '.html': 'text/html',
-  '.js': 'application/javascript',
-  '.jsx': 'application/javascript'
-}
+const BASE = path.join(process.cwd(), process.argv[2] || '.')
+const c = { plugins: [jsx, cp] }
+const mimedb = { '.html': 'text/html', '.js': 'application/javascript' }
 
 const server = http.createServer((req, res) => {
-  const fullPath = findFile(req.url === '/' ? '/index.html' : req.url)
-  if (!fullPath) return res.writeHead(404).end()
+  if (req.url === '/') req.url = '/index.html'
+  const fullPath = path.join(BASE, req.url)
+  if (!checkFile(fullPath)) return res.writeHead(404).end()
+  let ext = getExt(path.extname(fullPath))
   let contents = fs.readFileSync(fullPath, 'utf8')
-  const mime = mimedb[path.extname(fullPath)]
-  if (!mime) console.warn(`missing mime type for ${path.extname(fullPath)}`)
-  if (mime === 'application/javascript') {
-    const options = { filename: fullPath, plugins: [jsx] }
-    contents = babel.transformSync(contents, options).code
+  if (ext === '.json') [ext, contents] = ['.js', `export default ${contents}`]
+  if (ext === '.js') {
+    contents = babel.transformSync(contents, { filename: fullPath, ...c }).code
     const imports = lexer.parse(contents, fullPath)[0].reverse()
     imports.forEach(({ s, e }) => {
       const name = contents.substring(s, e)
-      contents = contents.slice(0, s) + updateDep(name) + contents.slice(e)
+      contents = contents.slice(0, s) + rewrite(name, req) + contents.slice(e)
     })
   }
+  const mime = mimedb[ext]
+  if (!mime) console.warn(`missing mime type for ${ext}`)
   if (mime) res.setHeader('Content-Type', mime)
   res.end(contents)
 })
 
-function findFile (url) {
-  const base = path.join(process.cwd(), srcDir, url)
-  if (checkFile(base)) return base
-  for (const ext in mimedb) {
-    if (checkFile(`${base}${ext}`)) return `${base}${ext}`
-    if (checkFile(`${base}/index${ext}`)) return `${base}/index${ext}`
-  }
-}
-
+const getExt = ext => (ext === '.jsx' ? '.js' : ext)
 const checkFile = p => fs.existsSync(p) && fs.lstatSync(p).isFile()
-const updateDep = n => (n[0] === '.' ? n : `https://cdn.pika.dev/${n}`)
+const rewrite = (name, { url }) => {
+  if (name[0] !== '.' && name[0] !== '/') return `https://cdn.pika.dev/${name}`
+  // TODO:: handle absolute paths better
+  const base = path.join(BASE, path.dirname(url), name)
+  if (checkFile(base)) return name
+  for (const ext of ['.js', '.jsx', '.json']) {
+    if (checkFile(`${base}${ext}`)) return `${name}${ext}`
+    if (checkFile(`${base}/index${ext}`)) return `${name}/index${ext}`
+  }
+  console.warn(`missing file "${name}" requested by "${path.join(BASE, url)}"`)
+  return name
+}
 
 lexer.init.then(() =>
   server.listen(3000, () => console.log('Listening on 3000'))
